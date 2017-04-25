5b5bded6167b25286cc423e4545e6b115a3141d3
Daniel Krezelok <daniel.krezelok@novartis.com>
2017-04-25 21:47:02 +0200

    ou jeee

--- CalenalaPushExtension/NotificationViewController.swift
+++ CalenalaPushExtension/NotificationViewController.swift
@@ -11,6 +11,7 @@ import UserNotifications
 import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
+    private static let BUTTON_OFF: UIImage = UIImage(named: "star-empty", in: nil, compatibleWith: nil)!
     private static let BUTTON_1: UIImage = UIImage(named: "star1-icon", in: nil, compatibleWith: nil)!
     private static let BUTTON_2: UIImage = UIImage(named: "star2-icon", in: nil, compatibleWith: nil)!
     private static let BUTTON_3: UIImage = UIImage(named: "star3-icon", in: nil, compatibleWith: nil)!
@@ -26,7 +27,19 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        commentField?.placeholder = NSLocalizedString("Tap here and write your comment", comment: "")        
+        commentField?.placeholder = NSLocalizedString("Tap here and write your comment", comment: "")
+
+        button1?.setImage(NotificationViewController.BUTTON_OFF, for: .normal)
+        button2?.setImage(NotificationViewController.BUTTON_OFF, for: .normal)
+        button3?.setImage(NotificationViewController.BUTTON_OFF, for: .normal)
+        button4?.setImage(NotificationViewController.BUTTON_OFF, for: .normal)
+        button5?.setImage(NotificationViewController.BUTTON_OFF, for: .normal)
+        
+        button1?.setImage(NotificationViewController.BUTTON_1, for: .selected)
+        button2?.setImage(NotificationViewController.BUTTON_2, for: .selected)
+        button3?.setImage(NotificationViewController.BUTTON_3, for: .selected)
+        button4?.setImage(NotificationViewController.BUTTON_4, for: .selected)
+        button5?.setImage(NotificationViewController.BUTTON_5, for: .selected)
     }
     
     func didReceive(_ notification: UNNotification) {
